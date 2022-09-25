e97caabff68867d6e426ff038a4cc2d74b096db7
henryfbp <HenryFBP@gmail.com>
2022-09-24 21:02:02 -0400

    format

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,14 @@
 // configureStore.js
 
-import { createStore } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
+import {createStore} from 'redux'
+import {persistStore, persistReducer} from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
 import rootReducer from './reducer'
 
 const persistConfig = {
-  key: 'root',
-  storage,
+    key: 'root',
+    storage,
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
